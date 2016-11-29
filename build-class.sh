7516b418dc6bf34c3f026f2c8910b43d44463991
Ezekiel Hendrickson <ezekielh@umich.edu>
2016-11-28 21:00:03 -0500

    packer: go back to not machine readable output

--- build-class.sh
+++ build-class.sh
@@ -7,4 +7,4 @@ else
 fi
 
 base=$(aws ec2 describe-images --owners self --filters "Name=tag:image_type,Values=base" --query 'Images[*].{Name:Name,ImageId:ImageId}' --output=text | sort -n -k2 | awk 'END { print $1 }')
-packer -machine-readable build -var "image_type=$class" -var "base_ami=$base" packer/template.json
+packer build -var "image_type=$class" -var "base_ami=$base" packer/template.json
