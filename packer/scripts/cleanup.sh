47138a7037b07536c2be08c2634fadd43c42473d
Ezekiel Hendrickson <ezekielh@umich.edu>
2016-12-07 17:19:29 -0500

    redis: more tested, probably better, cluster management

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 sudo yum -y clean all
+# `yum clean all` doesn't clean enough things
+sudo find /var/cache/yum -type f -delete 
 
+# no logs in the image
 sudo systemctl stop rsyslog
 sudo find /var/log -type f -delete
-# `yum clean all` doesn't clean enough things
-sudo find /var/cache/yum -type f -delete
+
+# no redis state in the image
+sudo systemctl stop redis
+sudo rm -f /var/lib/redis/nodes.conf
