e4f67475c55cc21b7f176a9a62c118714487f19c
Ezekiel Hendrickson <ezekielh@umich.edu>
2016-12-22 00:49:01 -0500

    more verbose output from packer cleanup script

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+echo "Cleaning yum..."
 sudo yum -y clean all
 # `yum clean all` doesn't clean enough things
-sudo find /var/cache/yum -type f -delete 
+sudo find /var/cache/yum -type f -delete
 
 # no logs in the image
+echo "Cleaning logs..."
 sudo systemctl stop rsyslog
-sudo find /var/log -type f -delete
+sudo find /var/log -type f -delete -print
 
 # no redis state in the image
-sudo systemctl stop redis
-sudo rm -f /var/lib/redis/nodes.conf
+echo "Cleaning redis state..."
+sudo systemctl status redis &>/dev/null && sudo systemctl stop redis
+sudo rm -fv /var/lib/redis/nodes.conf
