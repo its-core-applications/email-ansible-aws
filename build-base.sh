4f81dc28f38266787f176418031728cbd5ac61ff
Ezekiel Hendrickson <ezekielh@umich.edu>
2016-12-22 00:48:15 -0500

    fix AMI sorting in scripts

--- build-base.sh
+++ build-base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rhelbase=$(aws ec2 describe-images --filters "Name=owner-id,Values=309956199498" "Name=virtualization-type,Values=hvm" "Name=block-device-mapping.volume-type,Values=gp2" "Name=name,Values=RHEL-7.2_HVM_GA*" --query 'Images[*].{Name:Name,ImageId:ImageId}' --output=text | sort -n -k2 | awk 'END { print $1 }')
+rhelbase=$(aws ec2 describe-images --filters "Name=owner-id,Values=309956199498" "Name=virtualization-type,Values=hvm" "Name=block-device-mapping.volume-type,Values=gp2" "Name=name,Values=RHEL-7.2_HVM_GA*" --query 'Images[*].{Name:Name,ImageId:ImageId}' --output=text | sort -k2 | awk 'END { print $1 }')
 packer build -var "base_ami=$rhelbase" packer/template.json
 [[ $? -eq 0 ]] || exit 1
--- build-class.sh
+++ build-class.sh
@@ -6,5 +6,5 @@ else
     class=mx
 fi
 
-base=$(aws ec2 describe-images --owners self --filters "Name=tag:image_type,Values=base" --query 'Images[*].{Name:Name,ImageId:ImageId}' --output=text | sort -n -k2 | awk 'END { print $1 }')
+base=$(aws ec2 describe-images --owners self --filters "Name=tag:image_type,Values=base" --query 'Images[*].{Name:Name,ImageId:ImageId}' --output=text | sort -k2 | awk 'END { print $1 }')
 packer build -var "image_type=$class" -var "base_ami=$base" packer/template.json
