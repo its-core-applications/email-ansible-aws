4b8a5939d6d0d5faaa7528a683d48c34dcd22264
Ezekiel Hendrickson <ezekielh@umich.edu>
2017-01-11 12:21:33 -0500

    Bump EPEL release

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -2,5 +2,5 @@
 
 sudo yum-config-manager --enable rhui-REGION-rhel-server-optional
 sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
-sudo rpm -i http://mirrors.kernel.org/fedora-epel/7/x86_64/e/epel-release-7-8.noarch.rpm
+sudo rpm -i http://mirrors.kernel.org/fedora-epel/7/x86_64/e/epel-release-7-9.noarch.rpm
 sudo yum -y install yum-utils libselinux ansible
