254b21d9ba8c295147f0e4650114921db7e49685
Ezekiel Hendrickson <ezekielh@umich.edu>
2017-02-10 10:33:48 -0500

    Get rid of 'common' submodule
    It's more trouble than it's worth.

--- env-setup.sh
+++ env-setup.sh
@@ -9,7 +9,6 @@ hacking_dir=$(readlink -fn $(dirname "$BASH_SOURCE"))
 export ANSIBLE_CONFIG=$hacking_dir/ansible/ansible.cfg
 export ANSIBLE_PRIVATE_KEY_FILE=$hacking_dir/id_rsa
 export ANSIBLE_INVENTORY=$hacking_dir/inventory
-export ANSIBLE_ROLES_PATH=$hacking_dir/ansible/common/roles
 
 # Set up Vault
 export VAULT_ADDR='http://127.0.0.1:8200'
