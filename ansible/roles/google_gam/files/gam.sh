051c3c0a42d730c3d0b87602a8e3934b9e82d45d
Ezekiel Hendrickson <ezekielh@umich.edu>
2017-06-23 18:22:00 -0400

    Make gam wrapper runnable by the google user

--- ansible/roles/google_gam/files/gam.sh
+++ ansible/roles/google_gam/files/gam.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo -u google GAMUSERCONFIGDIR=/home/google/gam-config/umich.edu /home/google/GAM/src/gam.py "$@"
+if [[ $USER = 'google' ]]; then
+    GAMUSERCONFIGDIR=/home/google/gam-config/umich.edu /home/google/GAM/src/gam.py "$@"
+else
+    sudo -u google GAMUSERCONFIGDIR=/home/google/gam-config/umich.edu /home/google/GAM/src/gam.py "$@"
+fi
