20d1ef2bb059dfcbec6d512a0bc59be05207272b
Ezekiel Hendrickson <ezekielh@umich.edu>
2017-07-13 20:40:14 -0400

    packer: don't actually install ansible

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -1,6 +1,11 @@
-# Install things we need for ansible
-
+# Set up additional repositories
 sudo yum-config-manager --enable rhui-REGION-rhel-server-optional
 sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
-sudo rpm -i http://mirrors.kernel.org/fedora-epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+sudo rpm -i http://mirrors.kernel.org/fedora-epel/7/x86_64/e/epel-release-7-10.noarch.rpm
+
+
+# Install ansible and required packages
 sudo yum -y install yum-utils libselinux ansible
+
+# Remove ansible itself, just leaving the deps
+sudo yum -y remove ansible
