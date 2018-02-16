76f7394e83ac38873e07820768aa301bc5453743
Ezekiel Hendrickson <ezekielh@umich.edu>
2018-02-16 17:34:02 -0500

    Sync with latest version of https://github.com/ansible/ansible/pull/36081

--- ansible/filter_plugins/cidr_merge.py
+++ ansible/filter_plugins/cidr_merge.py
@@ -7,12 +7,24 @@ import types
 import netaddr
 from ansible import errors
 
-def cidr_merge(value):
-    if not isinstance(value, (list, tuple, types.GeneratorType)):
-        raise errors.AnsibleFilterError('cidr_merge: need sequence or iterable')
-    return [ str(ip) for ip in netaddr.cidr_merge(value) ]
-
 class FilterModule(object):
     def filters(self):
         return { 'cidr_merge': cidr_merge }
 
+def cidr_merge(value, action='merge'):
+    if not hasattr(value, '__iter__'):
+        raise errors.AnsibleFilterError('cidr_merge: expected iterable, got ' + repr(value))
+
+    if action == 'merge':
+        return [str(ip) for ip in netaddr.cidr_merge(value)]
+
+    elif action == 'span':                                                              # spanning_cidr needs at least two values
+        if len(value) == 0:
+            return None
+        elif len(value) == 1:
+            return str(netaddr.IPNetwork(value[0]))
+        else:
+            return str(netaddr.spanning_cidr(value))
+
+    else:
+        raise errors.AnsibleFilterError("cidr_merge: invalid action '%s'" % action)
