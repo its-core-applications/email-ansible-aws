84f5e6833e88d1f6452e2af0263adaeb42b38922
Ezekiel Hendrickson <ezekielh@umich.edu>
2018-07-13 13:56:25 -0400

    Get rid of gyb wrapper script

--- ansible/roles/google_gyb/files/99_gyb.sh
+++ ansible/roles/google_gyb/files/99_gyb.sh
@@ -1,3 +1,5 @@
-if [[ $USER != 'collaborate' ]]; then
-    alias gyb='sudo -u collaborate /usr/bin/gyb'
+if [[ $USER = 'collaborate' ]]; then
+    alias gyb='/usr/bin/python3 /home/collaborate/GYB/gyb.py --service-account'
+else
+    alias gyb='sudo -u collaborate /usr/bin/python3 /home/collaborate/GYB/gyb.py --service-account'
 fi
