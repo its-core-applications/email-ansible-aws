ed9efe228b6674a90deaddf01ed83fbd0d8370da
Rob Carleski <carleski@umich.edu>
2018-07-13 16:44:01 -0400

    Revert "Get rid of gyb wrapper script"
    This reverts commit 84f5e6833e88d1f6452e2af0263adaeb42b38922.

--- ansible/roles/google_gyb/files/99_gyb.sh
+++ ansible/roles/google_gyb/files/99_gyb.sh
@@ -1,5 +1,3 @@
-if [[ $USER = 'collaborate' ]]; then
-    alias gyb='/usr/bin/python3 /home/collaborate/GYB/gyb.py --service-account'
-else
-    alias gyb='sudo -u collaborate /usr/bin/python3 /home/collaborate/GYB/gyb.py --service-account'
+if [[ $USER != 'collaborate' ]]; then
+    alias gyb='sudo -u collaborate /usr/bin/gyb'
 fi
