d6bb0d019428112a9548a5d502a8f4c7c5d3d0e9
Ezekiel Hendrickson <ezekielh@umich.edu>
2018-07-19 14:20:13 -0400

    install libselinux-python
    This used to be pulled in automatically, but the latest AL2 seems not
    to.

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -2,7 +2,7 @@
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 # Install ansible and required packages
-sudo yum -y install yum-utils libselinux ansible
+sudo yum -y install yum-utils libselinux libselinux-python ansible
 
 # Remove ansible itself, just leaving the deps
 sudo yum -y remove ansible
