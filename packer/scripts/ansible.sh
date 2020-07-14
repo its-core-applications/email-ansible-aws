31a30fa611175861e1498d15ce7222da69dcec28
Ezekiel Hendrickson <ezekielh@umich.edu>
2020-07-14 15:41:17 -0400

    Fix pip3?

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -1,6 +1,10 @@
 # Set up additional repositories
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
+# Fix broken pip3
+# FIXME: this is an ugly hack
+sudo yum -y reinstall python3-pip
+
 # Install ansible and required packages
 sudo yum -y install yum-utils libselinux libselinux-python ansible
 
