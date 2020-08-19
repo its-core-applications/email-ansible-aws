e25862421302c8b23d898513135ae5d68e795790
Ezekiel Hendrickson <ezekielh@umich.edu>
2020-08-19 14:24:55 -0400

    Move to ansible-core 2.11 + collections
    * Move from flowerysong.sensu_go to sensu.sensu_go
    * Move from hashi_vault lookup to flowerysong.hvault.kv
    * Use custom AWS modules from flowerysong.melange, instead of using a
      forked Ansible.
    * Enable jinja2_native.
    * Remove manual generator unrolling.
    * Remove custom ControlPath setting.
    * Add some missing modes to file tasks (the provoking change was
      reverted, but it's better to be explicit regardless.)

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -1,12 +1,5 @@
 # Set up additional repositories
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-# Fix broken pip3
-# FIXME: this is an ugly hack
-sudo yum -y reinstall python3-pip
-
-# Install ansible and required packages
-sudo yum -y install yum-utils libselinux libselinux-python ansible
-
-# Remove ansible itself, just leaving the deps
-sudo yum -y remove ansible
+# Install prerequisite packages
+sudo yum -y install yum-utils libselinux libselinux-python
