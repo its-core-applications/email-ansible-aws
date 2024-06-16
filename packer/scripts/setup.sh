5788534572756a8c14c29bab50637fcad26ef04a
Ezekiel Hendrickson <ezekielh@umich.edu>
2024-06-16 03:24:30 -0400

    Upgrade rhui client before enabling
    Otherwise it ends up disabled when the upgrade occurs.

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -4,6 +4,7 @@ sudo /usr/bin/dnf -y localinstall katello-ca-consumer-latest.noarch.rpm
 sudo subscription-manager register --org="$SATELLITE_ORG" --activationkey="$SATELLITE_KEY"
 
 # Set up additional repositories
+sudo dnf -y upgrade rh-amazon-rhui-client
 sudo dnf config-manager --set-enabled codeready-builder-for-rhel-9-rhui-rpms
 sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 
